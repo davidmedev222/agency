bf07afe4d4759eb38f6f1c15dbdd0f34d5fbb3d3
davidmedev222 <davidmedev@gmail.com>
2023-04-11 17:12:49 -0300

    chore(service): add models for the projects

---
+++ src/models/project.models.ts
@@ -0,0 +1,27 @@
+interface ProjectPhotoURL {
+  id: number
+  url: string
+  alt: string
+}
+
+interface ProjectTestimony {
+  comment: string
+  photoURL: string
+  name: string
+  job: string
+}
+
+export interface Project {
+  id: number
+  title: string
+  category: string
+  description: string
+  videoURL: string
+  firstTitleTarget: string
+  firstDescriptionTarget: string
+  firstPhotosURL: ProjectPhotoURL[]
+  testimony: ProjectTestimony
+  secondTitleTarget: string
+  secondDescriptionTarget: string
+  secondPhotosURL: ProjectPhotoURL[]
+}
